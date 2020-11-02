6367911a11d225d99fff1d09ce5a803ff3a32c56
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2020-11-02 15:04:16 -0500

    Create spore sac items

---
+++ FungusCorpse.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace XRL.World.Parts
+{
+    using XRL.Language;
+
+    [Serializable]
+    [HasGameBasedStaticCache]
+    public class LimberFungusCorpse : Corpse
+    {
+        public override bool SameAs(IPart p) => false;
+
+        public override bool WantEvent(int ID, int cascade) => base.WantEvent(ID, cascade) || ID == BeforeDeathRemovalEvent.ID;
+
+        public override bool HandleEvent(BeforeDeathRemovalEvent E) {
+            string species = ParentObject.GetSpecies();
+            if (null != species) {
+                CorpseObject = GameObject.create(CorpseBlueprint);
+                var butcherable = CorpseObject.GetPart<Butcherable>();
+                var toButcher = butcherable.OnSuccess + Grammar.InitialCap(species);
+                if (GameObjectFactory.Factory.Blueprints.ContainsKey(toButcher)) {
+                    CorpseObject.GetPart<Butcherable>().OnSuccess = toButcher;
+                }
+            }
+            return base.HandleEvent(E);
+        }
+    }
+}
